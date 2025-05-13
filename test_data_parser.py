7a5df1d88881c42e6bfb314cfe9f36748fece37d
Samuel Chin <mystdreamer+git@gmail.com>
2025-05-13 18:13:05 +1000

    Add vic traffic signal volume data and updated unit test
    First file from may 2025 data set.

--- test_data_parser.py
+++ test_data_parser.py
@@ -5,11 +5,11 @@ from data_parser import parse_traffic_data
 
 class TestTrafficParser(unittest.TestCase):
     # This test checks that a valid 2025-format CSV can be parsed into a time-series DataFrame
-    # def test_parse_valid_csv(self):
-    #     df = parse_traffic_data("test_data/VSDATA_20250501.csv")
-    #     self.assertIsInstance(df, pd.DataFrame)
-    #     self.assertIn("SCATS", df.columns)
-    #     self.assertEqual(len(df.columns), 4)  # Expected columns: SCATS, Date, Time, Volume
+    def test_parse_valid_csv(self):
+        df = parse_traffic_data("test_data/VSDATA_20250501.csv")
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertIn("SCATS", df.columns)
+        self.assertEqual(len(df.columns), 4)  # Expected columns: SCATS, Date, Time, Volume
 
     # This test ensures the parser can successfully handle a 2006-format Excel file (.xlsx)
     def test_parse_valid_xlsx(self):
@@ -18,9 +18,9 @@ class TestTrafficParser(unittest.TestCase):
         self.assertEqual(len(df.columns), 4)
 
     # This test confirms that rows with zero vehicle counts are excluded when drop_zeros=True
-    # def test_drop_zeros(self):
-    #    df = parse_traffic_data("test_data/VSDATA_20250501.csv", drop_zeros=True)
-    #    self.assertTrue((df["Volume"] != 0).all())
+    def test_drop_zeros(self):
+       df = parse_traffic_data("test_data/VSDATA_20250501.csv", drop_zeros=True)
+       self.assertTrue((df["Volume"] != 0).all())
 
     # This test ensures unsupported file types raise a clear ValueError
     def test_reject_invalid_filetype(self):
